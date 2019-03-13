cf0b223b58d6127667981c9627eb9402e9b335ca
kamal johnson <kamaljohnson12345@gmail.com>
2019-03-13 11:35:56 +0530

    spawn enemie blocks/block movement

---
+++ Game/Assets/Reference/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static bool IsPlaying;
+    public static bool IsDead;
+    public static int Score;
+    public static float DistanceTravelled;
+
+    public static Transform PlayerTransform;
+    public static float Speed = 10;
+
+    public void Start()
+    {
+        IsPlaying = true;
+        IsDead = false;
+        PlayerTransform = GetComponent<Transform>();
+    }
+}
